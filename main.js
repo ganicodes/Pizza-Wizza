69dd482a5c62e0a6f3dfe83873ec067c454bb128
Gani <abdulgani.work@gmail.com>
2021-09-11 19:21:24 +0530

    Second commit- added featured pizzas page, footer and made it responsive-- need to add naviagation menu for mobile devices

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+var loginButton = document.getElementById("login-form");
+var registerButton = document.getElementById("registeration-form");
+var toggleButton = document.getElementById("btn");
 
+function register() {
+  loginButton.style.left = "-400px";
+  registerButton.style.left = "50px";
+  toggleButton.style.left = "110px";
+}
+function login() {
+  loginButton.style.left = "50px";
+  registerButton.style.left = "450px";
+  toggleButton.style.left = "0";
+}
